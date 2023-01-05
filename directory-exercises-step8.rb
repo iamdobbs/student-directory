6171c9c6dfdd4b32ea68542964106ae65bca8908
Hayley Dobbs <iamdobbs@icloud.com>
2023-01-05 14:25:56 +0000

    Added additional student information - hobbies & birthplace

--- directory-exercises-step8.rb
+++ directory-exercises-step8.rb
@@ -1,17 +1,19 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students, hit return and enter their place of birth"
+  puts "To finish, just hit return twice more"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
+  birthplace = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobbies: :murder, birthplace: birthplace}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
+    birthplace = gets.chomp
   end
   # return the array of students
   students
@@ -26,7 +28,8 @@ def print(students)
     c = 0
     i = 0
     while c != students.count do
-    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort). Hobbies include: #{students[i][:hobbies]}. 
+    Place of birth: #{students[i][:birthplace]}"
     i += 1
     c += 1
   end  
