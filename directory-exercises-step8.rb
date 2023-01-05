afc71d24e56221c3ccca13d525a2253c94f3e93f
Hayley Dobbs <iamdobbs@icloud.com>
2023-01-05 14:00:27 +0000

    Amended to only print names shorter than 12 characters

--- directory-exercises-step8.rb
+++ directory-exercises-step8.rb
@@ -15,7 +15,8 @@ def input_students
   end
   # return the array of students
   students
-end  
+end   
+
 
 def print_header
   puts "The students of Villains Academy"
@@ -23,8 +24,10 @@ def print_header
 end
 
 def print(students)
-  students.each.with_index(1) do |student, index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+  students.select do |student|
+    if student[:name].length < 12
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end  
 end 
   
@@ -39,3 +42,4 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
+
